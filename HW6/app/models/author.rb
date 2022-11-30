5cb9f55187ccbe5fc18c3abcfedca476cf275355
Haidamac <serge.krinitsa@gmail.com>
2022-12-01 00:18:26 +0200

    redundant code removed

--- HW6/app/models/author.rb
+++ HW6/app/models/author.rb
@@ -1,5 +1,4 @@
 class Author < ApplicationRecord
-  #has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy #, through: :articles
   validates :name, presence: { message: "Must be given please" }, uniqueness: { message: "This name already taken" }
 end
