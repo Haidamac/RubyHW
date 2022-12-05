eeaa219e94cbd91fd2d7e54736643b039c399d2b
Haidamac <serge.krinitsa@gmail.com>
2022-12-06 01:00:29 +0200

    likes controller bugs fixed

--- HW7/app/controllers/api/v1/likes_controller.rb
+++ HW7/app/controllers/api/v1/likes_controller.rb
@@ -1,23 +1,28 @@
 class Api::V1::LikesController < ApplicationController
 
   def create
-    @author = Author.find(params[:author_id])
-    @like = @author.likes.new(like_params)
-    @like.save
-    redirect_back(fallback_location: root_path)
+    if params[:article_id].present?
+      @article = Article.find(params[:article_id])
+      @like_article = @article.likes.create(like_params)
+      render json: @like_article, status: :ok
+    else
+      @comment = Comment.find(params[:comment_id])
+      @like_comment = @comment.likes.create(like_params)
+      render json: @like_comment, status: :ok
+    end
+
   end
 
   def destroy
-    @author = Author.find(params[:author_id])
-    @like = @author.likes.find(params[:id])
+    @like = Like.find(params[:id])
     @like.destroy
-    redirect_back(fallback_location: root_path)
+    render json: { message: 'Unliked' }
   end
 
   private
 
   def like_params
-    params.require(:like).permit(:likeable_id, :likeable_type)
+    params.require(:like).permit(:author_id)
   end
 
 end
