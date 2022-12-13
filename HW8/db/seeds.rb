9cf6638579dbb29dd74dabe47e97a094421075af
Haidamac <serge.krinitsa@gmail.com>
2022-12-14 00:51:04 +0200

    seeds.rb added

--- HW8/db/seeds.rb
+++ HW8/db/seeds.rb
@@ -1,44 +1,53 @@
-authors = 5.times do
-  Author.create({ name: Faker::ProgrammingLanguage.creator })
-end
-
-tags = 10.times do
+10.times do
   Tag.create({ tagname: Faker::ProgrammingLanguage.name })
 end
 
-articles1 = 5.times do
-  Article.create({ title: Faker::ProgrammingLanguage.name, body:  Faker::Quote.matz, tag_ids: [1, 4, 7], author_id: 1 })
+3.times do
+  author = Author.create({ name: Faker::ProgrammingLanguage.creator })
+  2.times do
+    article = Article.create({ title: Faker::ProgrammingLanguage.name, body:  Faker::Quote.matz, author_id: author.id, tag_ids: [1, 3, 5] })
+    15.times do
+      Comment.create({ body: Faker::Quote.jack_handey, author_id: author.id, article_id: article.id })
+    end
+  end
 end
 
-articles2 = 5.times do
-  Article.create({ title: Faker::ProgrammingLanguage.name, body:  Faker::Quote.matz, tag_ids: [2, 5, 8], author_id: 2 })
+5.times do
+  author = Author.create({ name: Faker::ProgrammingLanguage.creator })
+  3.times do
+    article = Article.create({ title: Faker::ProgrammingLanguage.name, body:  Faker::Quote.matz, author_id: author.id, tag_ids: 8 })
+    20.times do
+      Comment.create({ body: Faker::Quote.jack_handey, author_id: author.id, article_id: article.id })
+    end
+  end
 end
 
-articles3 = 5.times do
-  Article.create({ title: Faker::ProgrammingLanguage.name, body:  Faker::Quote.matz, tag_ids: [3, 9], author_id: 3 })
+7.times do
+  author = Author.create({ name: Faker::ProgrammingLanguage.creator })
+  5.times do
+    article = Article.create({ title: Faker::ProgrammingLanguage.name, body:  Faker::Quote.matz, author_id: author.id, tag_ids: 1 })
+    18.times do
+      Comment.create({ body: Faker::Quote.jack_handey, author_id: author.id, article_id: article.id })
+    end
+  end
 end
 
-articles4 = 5.times do
-  Article.create({ title: Faker::ProgrammingLanguage.name, body:  Faker::Quote.matz, tag_ids: [1, 5], author_id: 4 })
+3.times do
+  author = Author.create({ name: Faker::ProgrammingLanguage.creator })
+  8.times do
+    article = Article.create({ title: Faker::ProgrammingLanguage.name, body:  Faker::Quote.matz, author_id: author.id, tag_ids: 6 })
+    25.times do
+      Comment.create({ body: Faker::Quote.jack_handey, author_id: author.id, article_id: article.id })
+    end
+  end
 end
 
-
-comment = 5.times do
-  Comment.create({ body: Faker::Quote.jack_handey, author_id: 3, article_id: 1 })
-end
-
-comment = 3.times do
-  Comment.create({ body: Faker::Quote.jack_handey, author_id: 1, article_id: 3 })
-end
-
-comment = 4.times do
-  Comment.create({ body: Faker::Quote.jack_handey, author_id: 2, article_id: 5 })
-end
-
-comment = 3.times do
-  Comment.create({ body: Faker::Quote.yoda, author_id: 4, article_id: 7 })
-end
-
-comment = 5.times do
-  Comment.create({ body: Faker::Quote.yoda, author_id: 5, article_id: 2 })
+2.times do
+  author = Author.create({ name: Faker::ProgrammingLanguage.creator })
+  3.times do
+    article = Article.create({ title: Faker::ProgrammingLanguage.name, body:  Faker::Quote.matz, author_id: author.id, tag_ids: 5 })
+    15.times do
+      Comment.create({ body: Faker::Quote.jack_handey, author_id: author.id, article_id: article.id })
+    end
+  end
 end
