6b888b72594cb2fb306f2331f98adb5de6a06b8d
Haidamac <serge.krinitsa@gmail.com>
2022-12-23 23:04:18 +0200

    rubocop refactor

--- HW5/app/controllers/api/v1/articles_controller.rb
+++ HW5/app/controllers/api/v1/articles_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show update destroy ]
+  before_action :set_article, only: %i[show update destroy]
 
   # GET /api/v1/articles
   def index
     @articles = Article.all
 
-    if @articles 
+    if @articles
       render json: { data: @articles }, status: :ok
     else
       render json: @articles.errors, status: :bad_request
@@ -14,18 +14,15 @@ class Api::V1::ArticlesController < ApplicationController
 
   # GET /api/v1/articles/1
   def show
-    #set_article
-    # @article = Article.find(params[:id])
-    render json: {data: @article}, status: :ok
-   
+    render json: { data: @article }, status: :ok
   end
 
   # POST /api/v1/articles
   def create
     @article = Article.new(article_params)
 
-    if @article.save 
-      render json: { status: "Created", data: @article }, status: :created
+    if @article.save
+      render json: { status: 'Created', data: @article }, status: :created
     else
       render json: @article.errors, status: :unprocessable_entity
     end
@@ -33,10 +30,8 @@ class Api::V1::ArticlesController < ApplicationController
 
   # PATCH/PUT /api/v1/articles/1
   def update
-    #set_article
-    # @article = Article.find(params[:id])
     if @article.update(article_params)
-      render json: { status: "Update", data: @article }, status: :ok
+      render json: { status: 'Update', data: @article }, status: :ok
     else
       render json: @article.errors, status: :unprocessable_entity
     end
@@ -44,10 +39,8 @@ class Api::V1::ArticlesController < ApplicationController
 
   # DELETE /api/v1/articles/1
   def destroy
-    #set_article
-    # @article = Article.find(params[:id])
-    if @article.destroy! 
-      render json: { status: "Delete" }, status: :ok
+    if @article.destroy!
+      render json: { status: 'Delete' }, status: :ok
     else
       render json: @article.errors, status: :unprocessable_entity
     end
--- HW5/config/routes.rb
+++ HW5/config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :articles
-      #post '/articles', to: 'articles#create'
-      #get '/articles', to: 'articles#index'
-      #get '/articles/:id', to: 'articles#show'
-      #patch '/articles/:id', to: 'articles#update'
-      #delete '/articles/:id', to: 'articles#destroy'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
