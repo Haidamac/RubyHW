25d0931f0eead30986ca65b9066ccb98024a6f37
Haidamac <serge.krinitsa@gmail.com>
2022-12-27 21:58:58 +0200

    HW10 update

--- HW10/db/seeds.rb
+++ HW10/db/seeds.rb
@@ -1,47 +1,42 @@
-# Product.destroy_all
-# Category.destroy_all
-# User.destroy_all
-# AdminUser.destroy_all
-
 Category.create(title: 'Gin')
 Category.create(title: 'Whiskey')
 Category.create(title: 'Beer')
 Category.create(title: 'Wine')
 
-Product.create(name: 'Gordon\'s',
-    description: 'Gin Gordon\'s 1l 37.5% London Dry',
+Product.create(name: 'Gordon\'s London Dry Gin',
+    description: 'Gordon\'s London Dry Gin 1l 37.5% Same great award-winning gin as the classic green Gordon\'s bottle, different label & bottle to celebrate the first export out of the UK in the early 1900\'s.',
     image: "https://winetime.com.ua/uploads/public/goods/2940/1581953191_49059_502_378.webp",
     price: 14.88,
     category_id: 1)
 Product.create(name: 'Jack Daniel\'s Old No.7',
-    description: 'Tennessee Sour Mash Whiskey Jack Daniel\'s Old No.7 0.7l 40%',
+    description: 'Tennessee Sour Mash Whiskey Jack Daniel\'s Old No.7 0.7l 40% Mellowed drop by drop through 10-feet of sugar maple charcoal, then matured in handcrafted barrels of our own making. And our Tennessee Whiskey doesn’t follow a calendar. It’s only ready when our tasters say it is.',
     image: "https://winetime.com.ua/uploads/public/goods/1575/1509805203_49532_502_378.webp",
     price: 18.99,
     category_id: 2)
 Product.create(name: 'Jim Beam White',
-    description: 'Kentucky Straight Bourbon Whiskey Jim Beam White 0.7l 40%',
+    description: 'Kentucky Straight Bourbon Whiskey Jim Beam White 0.7l 40% JIM BEAM WHITE derives its rich satisfying flavour from a treasured family recipe and a minimum of four years of ageing in new, charred oak barrels.',
     image: "https://winetime.com.ua/uploads/public/goods/16125/52233305134_30e5058d3c_b_502_378.webp",
     price: 15.59,
     category_id: 2)
 Product.create(name: 'Fantinel Prosecco',
-    description: 'Fantinel Prosecco Extra Dry 1,5l 11.5% wine',
+    description: 'Fantinel Prosecco Extra Dry 1,5l 11.5% Once upon a time there was a man with a radiant smile who venerated good food so much that it became his reason for living. And in such a joyful parade of pleasure, wine sat on the royal throne.',
     image: "https://winetime.com.ua/uploads/public/goods/3538/1536585859_75684_502_378.webp",
     price: 39.19,
     category_id: 4)
-Product.create(name: 'Latinium Sparkling Breeze',
-    description: 'Latinium Sparkling Breeze White Semidry 0.75l 8.5% wine',
-    image: "https://content1.rozetka.com.ua/goods/images/big/11996090.jpg",
-    price: 4.29,
+Product.create(name: 'Dow\'s 20 Years Old Tawny Port',
+    description: 'Dow\'s 20 Years Old Tawny Port 0.75l 20% 93 pts Wine Spectator - For many the perfect age for a great Tawny Port. The vibrant purple colour of a great young Port has softened to a delicate green-gold after 20 long years in seasoned oak casks under the watchful eye of the master blender and the Dow’s head lodge man.',
+    image: "https://winetime.com.ua/uploads/public/goods/275/52419864226_f0b7f969d3_b_502_378.webp",
+    price: 32.59,
     category_id: 4)
 Product.create(name: 'Martini Asti',
-    description: 'Martini Asti White Sweet 0.75l 7.5% wine',
+    description: 'Martini Asti White Sweet 0.75l 7.5% wine Martini & Rossi is the leading Italian sparkling wine producer. We harvest and store the fresh grape juice of Moscato Bianco grapes, harvested in the Asti DOC area. We arrest fermentation to keep the natural sweetness of the ripe musts that give Martini Asti its taste and aroma of grape juice, vine peaches, elderberry and sage.',
     image: "https://winetime.com.ua/uploads/public/goods/2943/1525691794_49474_502_378.webp",
     price: 9.19,
     category_id: 4)
-Product.create(name: 'Browar Beer',
-    description: 'Browar Beer 12x0.35l',
-    image: "https://content.rozetka.com.ua/goods/images/big/302605668.jpg",
-    price: 15.99,
+Product.create(name: 'Paulaner Hefe-Weissbier Naturtrub',
+    description: 'Paulaner Hefe-Weissbier Naturtrub 0.5l It’s the #1 wheat beer in Germany and one of the world’s favorites. In fact, beer experts call it a masterpiece. In developing this beer, the Paulaner brewmasters have perfected a unique technique with “yeast suspension,” resulting in a uniform, slightly cloudy appearance, consistent quality, and perfect taste.',
+    image: "https://winetime.com.ua/uploads/public/goods/2959/51237484431_4a44462f11_b_502_378.webp",
+    price: 2.39,
     category_id: 3)
 Product.create(name: 'Whitley Neill Sloe Gin',
     description: 'Whitley Neill Sloe Gin 0.7l 28% Whitley Neill Gin is a handcrafted, premium gin that is inspired by the very best flavours and ingredients from around the world – and distilled in the heart of the City of London. Discover what makes our range of artisanal gins',
@@ -49,34 +44,34 @@ Product.create(name: 'Whitley Neill Sloe Gin',
     price: 20.79,
     category_id: 1)
 Product.create(name: 'Sour Cherry Gin',
-    description: 'Luxardo Sour Cherry Gin 0.7l 37.5%',
+    description: 'Luxardo Sour Cherry Gin 0.7l 37.5% Deep red in color, Luxardo Sour Cherry Gin has a pungent marasca cherry aroma with intense juniper notes. The initial taste is typical of gin with spicy and long lasting sour aftertaste. Luxardo Sour Cherry Gin can be used to twist many classic cocktails such as the Martinez and the Gin Fizz, or it can be enjoyed neat or over ice as an after dinner drink.',
     image: "https://winetime.com.ua/uploads/public/goods/14353/51478552508_93e952c6fc_b_502_378.webp",
     price: 19.99,
     category_id: 1)
 Product.create(name: 'The Pogues',
-    description: 'The Pogues Irish Whiskey 1l 40%',
+    description: 'The Pogues Irish Whiskey 1l 40% A unique Irish Whiskey created with a passion by master whiskey maker Frank McHardy. Exceptionally smooth; enjoy it neat, with ice, with a mixer, in a cocktail, or however you damn well want.',
     image: "https://winetime.com.ua/uploads/public/goods/11358/51171780096_563d090b48_b_502_378.webp",
     price: 31.19,
     category_id: 2)
 Product.create(name: 'Tanqueray',
-    description: 'Tanqueray London Dry Gin 0.7l 47.3%',
+    description: 'Tanqueray London Dry Gin 0.7l 47.3% Fortune favours the brave. Back in the 1830´s Charles Tanqueray wasn\'t afraid to mix his bold ideas. His ingenious pursuit for perfection paid off, creating Tanqueray London Dry, a perfectly balanced gin and one of the most awarded gins in the world.',
     image: "https://winetime.com.ua/uploads/public/goods/15961/52130241702_f825547d9a_b_502_378.webp",
     price: 13.59,
     category_id: 1)
 Product.create(name: 'Finsbury Platinum',
-    description: 'Finsbury Platinum Gin 0.7l 47%',
+    description: 'Finsbury Platinum Gin 0.7l 47% The Finsbury Platinum London Dry Gin is a steeped in tradition English Gin, which spoils the sense of taste with fruity and easy spicy flavours',
     image: "https://winetime.com.ua/uploads/public/goods/6462/1383317069_5664_502_378.webp",
     price: 16.59,
     category_id: 1)
 Product.create(name: 'Glenmorangie Nectar',
-    description: 'Glenmorangie Nectar Whiskey d\'Or 0.7 л 46%',
+    description: 'Glenmorangie Nectar Whiskey d\'Or 0.7 л 46% With this malt from the core series, Highland distillery Glenmorangie creates a solid \'whisky for everyday life\'. Easy to drink, partly due to the finish in sauternes casks. This \'Golden Nectar\' is 12 years old and bottled at 46%.',
     image: "https://winetime.com.ua/uploads/public/goods/6905/1441103025_55335_502_378.webp",
     price: 59.99,
     category_id: 2)
 Product.create(name: 'Wieninger Weissbier Hell',
-    description: 'Wieninger Weissbier Hell 5.3% 0.5l',
+    description: 'Wieninger Weissbier Hell 5.3% 0.5l Eine altbayerische Weißbierspezialität. Süffig und natürlich mit Original Wieninger Naturhefe aus eigener Reinzucht. Wieninger Biere sind naturbelassen und nicht pasteurisiert.',
     image: "https://winetime.com.ua/uploads/public/goods/15337/51781796161_d588de57e9_b_502_378.webp",
-    price: 42.79,
+    price: 2.19,
     category_id: 3)
 Product.create(name: 'City of London Old Tom Gin',
     description: 'City of London Distillery Old Tom Gin 0.7l 40.3% City of London Old Tom Gin is a lightly sweetened Old Tom style gin. Bottled at 43% ABV, it features a very traditional list of botanicals. Each batch though is small and tightly controlled— produced a scant 200 bottles at a time.',
