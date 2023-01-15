37963fffcce5f5e7638e8e77400ebf5289474629
Haidamac <serge.krinitsa@gmail.com>
2023-01-16 00:41:43 +0200

    moved logic from views to helpers

--- HW12/app/helpers/orders_helper.rb
+++ HW12/app/helpers/orders_helper.rb
@@ -1,10 +1,28 @@
 module OrdersHelper
 
+  def order_date(order)
+    if order.paid?
+      "Date of purchase #{order.updated_at.to_date}"
+    else
+      "Order date #{order.created_at.to_date}"
+    end
+  end
+
   def order_status_view(order)
     if order.paid?
       "Date of purchase #{order.updated_at.to_date}"
     else
       link_to "Order date #{order.created_at.to_date} Please, repeat your order!", root_path
+    end
+  end
+
+  def order_status_button(order)
+    if order.paid?
+      link_to 'paid', order_paid_path(order), class: 'btn btn-outline-success btn-sm'
+    elsif Time.now < order.created_at + 1.day
+      link_to 'unpaid', order_path(order), class: 'btn btn-outline-danger btn-sm'
+    else
+      link_to 'canceled', order_path(order), class: 'btn btn-outline-secondary btn-sm'
     end
   end
 end
--- HW12/app/helpers/products_helper.rb
+++ HW12/app/helpers/products_helper.rb
@@ -1,2 +1,24 @@
 module ProductsHelper
+
+  def add_cart(product)
+    if current_user
+      if !current_order.line_items.where(product_id: product.id).present?
+        # Add to Cart
+        button_to 'Add to cart', line_items_path(product_id: product.id), class: 'btn btn-light btn-sm'
+      else
+        # if product is in cart - checkout
+        button_to 'Checkout', current_order_path, method: :get, class: 'btn btn-light btn-sm'
+      end
+    end
+  end
+
+  def add_cart_product(product)
+    if current_user
+      if !current_order.line_items.where(product_id: @product.id).present?
+        button_to 'Add to cart', line_items_path(product_id: @product.id), class: 'btn btn-outline-primary'
+      else
+        button_to 'Checkout', current_order_path, method: :get, class: 'btn btn-outline-primary'
+      end
+    end
+  end
 end
