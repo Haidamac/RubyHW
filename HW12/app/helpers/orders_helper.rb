aadb0eb5e25f3a24a07c04273b2fd8776253b089
Haidamac <serge.krinitsa@gmail.com>
2023-01-15 20:05:01 +0200

    moved some logic to helpers

--- HW12/app/helpers/orders_helper.rb
+++ HW12/app/helpers/orders_helper.rb
@@ -1,2 +1,10 @@
 module OrdersHelper
+
+  def order_status_view(order)
+    if order.paid?
+      "Date of purchase #{order.updated_at.to_date}"
+    else
+      link_to "Order date #{order.created_at.to_date} Please, repeat your order!", root_path
+    end
+  end
 end
