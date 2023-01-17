998feedb2233f9765468e604747dc4b3b6ed4b8b
Haidamac <serge.krinitsa@gmail.com>
2023-01-17 08:35:33 +0200

    rubocop refactoring

--- HW12/config/routes.rb
+++ HW12/config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
@@ -15,10 +17,6 @@ Rails.application.routes.draw do
   get '/current_order', to: 'orders#show_current', as: :current_order
   resources :orders, only: %i[index show]
   resources :line_items, only: %i[create update destroy]
-  # post 'line_items' => 'line_items#create'
-  # get 'line_items/:id' => 'line_items#destroy', as: 'line_item'
-  # get 'line_items/:id/add' => 'line_items#add_quantity', as: 'line_item_add'
-  # get 'line_items/:id/reduce' => 'line_items#reduce_quantity', as: 'line_item_reduce'
   get 'orders/:id/pay_details' => 'orders#pay_details', as: 'order_pay_details'
   put 'orders/:id/pay' => 'orders#pay', as: 'order_pay'
   get 'orders/:id/paid' => 'orders#paid', as: 'order_paid'
