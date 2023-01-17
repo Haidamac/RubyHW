9ddafbd047893911a967c2f32e4454244471fb9a
Haidamac <serge.krinitsa@gmail.com>
2023-01-17 09:18:05 +0200

    eager loading fixed

--- HW12/app/admin/orders.rb
+++ HW12/app/admin/orders.rb
@@ -6,7 +6,8 @@ ActiveAdmin.register Order do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :cart_id, :user_id, :status
+  permit_params :user_id, :status
+  includes :user
   #
   # or
   #
