9b043a830b6681c08fdbb8fff6bd8888b54978c2
Haidamac <serge.krinitsa@gmail.com>
2023-02-06 09:27:13 +0200

    bug with line_item destroy fixed

--- HW15/app/controllers/line_items_controller.rb
+++ HW15/app/controllers/line_items_controller.rb
@@ -2,7 +2,7 @@
 
 class LineItemsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_line_item, only: %i[show update update_current destroy]
+  before_action :set_line_item, only: %i[show update update_order destroy]
 
   def show; end
 
@@ -18,25 +18,16 @@ class LineItemsController < ApplicationController
 
     current_order.unpaid!
     redirect_to line_item_path(id: @line_item.id), notice: "#{product.name} was successfully added to the cart"
-    # redirect_to current_order_path, notice: "#{product.name} was successfully added to the cart"
   end
 
   def update
-    if params[:change_quantity] == 'add'
-      @line_item.update(quantity: @line_item.quantity + 1)
-    else
-      @line_item.quantity > 1 ? @line_item.update(quantity: @line_item.quantity - 1) : @line_item.destroy
-    end
+    change_quantity
 
     redirect_to line_item_path(id: @line_item.id)
   end
 
-  def update_current
-    if params[:change_quantity] == 'add'
-      @line_item.update(quantity: @line_item.quantity + 1)
-    else
-      @line_item.quantity > 1 ? @line_item.update(quantity: @line_item.quantity - 1) : @line_item.destroy
-    end
+  def update_order
+    change_quantity
 
     redirect_back(fallback_location: current_order)
   end
@@ -52,6 +43,14 @@ class LineItemsController < ApplicationController
     @line_item = LineItem.includes(:product).find(params[:id])
   rescue ActiveRecord::RecordNotFound => e
     logger.info e
-    redirect_to current_order_path, method: :get, notice: 'Ooops! Please try again'
+    redirect_to root_path, method: :get
+  end
+
+  def change_quantity
+    if params[:change_quantity] == 'add'
+      @line_item.update(quantity: @line_item.quantity + 1)
+    else
+      @line_item.quantity > 1 ? @line_item.update(quantity: @line_item.quantity - 1) : @line_item.destroy
+    end
   end
 end
--- HW15/config/routes.rb
+++ HW15/config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   get '/current_order', to: 'orders#show_current', as: :current_order
   resources :orders, only: %i[index show]
   resources :line_items, only: %i[show create update destroy]
-  put 'line_items/:id/update_current' => 'line_items#update_current', as: 'line_item_order'
+  put 'line_items/:id/update_order' => 'line_items#update_order', as: 'line_item_order'
   get 'orders/:id/pay_details' => 'orders#pay_details', as: 'order_pay_details'
   put 'orders/:id/pay' => 'orders#pay', as: 'order_pay'
   get 'orders/:id/paid' => 'orders#paid', as: 'order_paid'
