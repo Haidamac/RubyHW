1cd9d1d6dbb48e4bc0282b4f1c96ad5ea8dfef2d
Haidamac <serge.krinitsa@gmail.com>
2023-02-27 20:56:57 +0200

    add factories configuration

---
+++ HW18/spec/factories.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory(:product) do
+    name { Faker::Music::RockBand.name }
+    description { Faker::Music::RockBand.song }
+    price { rand(0.99..24.99) }
+    category_id { rand(1..4) }
+  end
+end
