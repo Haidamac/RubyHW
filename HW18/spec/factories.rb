883d0ddf0bad6c67b18f3ecf3c755689e63f350f
Haidamac <serge.krinitsa@gmail.com>
2023-02-28 22:20:25 +0200

    tests refactoring

--- HW18/spec/factories.rb
+++ HW18/spec/factories.rb
@@ -3,12 +3,12 @@
 FactoryBot.define do
   factory :category do
     title { Faker::Music.genre }
-  end
 
-  factory :product do
-    name { Faker::Music::RockBand.name }
-    description { Faker::Music::RockBand.song }
-    price { rand(0.99..24.99) }
-    category_id { rand(1..4) }
+    factory :product do
+      name { Faker::Music::RockBand.name }
+      description { Faker::Music::RockBand.song }
+      price { Faker::Number.decimal }
+      category_id { Category.first.id || association(:category.id) }
+    end
   end
 end
