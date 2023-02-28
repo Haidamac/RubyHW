8403bfbe67944af113258be9c9a6a8e5fea4d6ef
Haidamac <serge.krinitsa@gmail.com>
2023-02-28 22:56:41 +0200

    association to factory bot added

--- HW18/spec/factories.rb
+++ HW18/spec/factories.rb
@@ -3,12 +3,13 @@
 FactoryBot.define do
   factory :category do
     title { Faker::Music.genre }
+  end
 
-    factory :product do
-      name { Faker::Music::RockBand.name }
-      description { Faker::Music::RockBand.song }
-      price { Faker::Number.decimal }
-      category_id { Category.first.id || association(:category.id) }
-    end
+  factory :product do
+    association :category
+    name { Faker::Music::RockBand.name }
+    description { Faker::Music::RockBand.song }
+    price { Faker::Number.decimal }
+    category_id { Category.first.id || association(:category.id) }
   end
 end
