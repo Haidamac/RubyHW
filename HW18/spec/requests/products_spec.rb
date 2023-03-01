0fc14bc84dfcda45643e823a5dc80962ebcab6c4
Haidamac <serge.krinitsa@gmail.com>
2023-03-01 22:30:02 +0200

    add first test to products controller

---
+++ HW18/spec/requests/products_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe ProductsController, type: :request do
+  describe 'GET /products' do
+    it 'it returns status 200' do
+      get products_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
