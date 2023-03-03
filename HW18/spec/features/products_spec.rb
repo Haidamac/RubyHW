f30328aa5fb61e30f24d389be3d3b731849ebb16
Haidamac <serge.krinitsa@gmail.com>
2023-03-03 21:33:16 +0200

    add featured tests for products

--- HW18/spec/features/products_spec.rb
+++ HW18/spec/features/products_spec.rb
@@ -10,5 +10,17 @@ feature 'products' do
       visit products_path
       expect(page).to have_content('Dionysus Store')
     end
+
+    scenario 'adding product' do
+      visit products_path
+      click_button 'Add'
+      expect(page).to have_content('In')
+    end
+
+    scenario 'open product show page' do
+      visit products_path(product)
+      expect(page).to have_current_path(products_path(product))
+      expect(page).to have_content('Price:')
+    end
   end
 end
