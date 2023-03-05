e5acbf515f38976dd4cfdfc27951b5bc8acb5803
Haidamac <serge.krinitsa@gmail.com>
2023-03-05 21:44:05 +0200

    add featured tests for sign up user

--- HW18/spec/features/products_spec.rb
+++ HW18/spec/features/products_spec.rb
@@ -6,21 +6,33 @@ feature 'products' do
   let(:product) { create(:product) }
 
   context 'as visitor' do
-    scenario 'viewing index page' do
+    before do
       visit products_path
-      expect(page).to have_content('Dionysus Store')
     end
 
-    scenario 'adding product' do
-      visit products_path
-      click_button 'Add'
-      expect(page).to have_content('In')
+    scenario 'viewing index page' do
+      expect(page).to have_content('Dionysus Store')
     end
 
     scenario 'open product show page' do
       visit products_path(product)
       expect(page).to have_current_path(products_path(product))
       expect(page).to have_content('Price:')
+    end
+
+    scenario 'sign up' do
+      click_link 'Sign Up'
+      expect(page).to have_current_path(new_user_registration_path)
+      fill_in 'Email', with: 'new_user@bla.net'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_button 'Sign up'
+      expect(page).to have_content('Welcome! You have signed up successfully')
+    end
+
+    scenario 'adding product' do
+      click_button 'Add'
+      expect(page).to have_content('In')
     end
   end
 end
